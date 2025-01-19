7e410af1cecc03d8c2e43bde2718153453875e75
wwwxxch <chihhui.x@gmail.com>
2025-01-19 17:43:16 +0800

    change to use pinecone & add other input docs

--- vectorDB/createVectorStore.py
+++ vectorDB/createVectorStore.py
@@ -1,7 +1,11 @@
 from langchain_community.document_loaders import DirectoryLoader, TextLoader, CSVLoader
+from langchain_unstructured import UnstructuredLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_chroma.vectorstores import Chroma
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
+
+# from langchain_chroma.vectorstores import Chroma
 import logging
 import os
 
@@ -23,21 +27,33 @@ def load_documents(directory_path: str):
     )
 
     # csv
-    csv_loader = DirectoryLoader(
-        directory_path,
-        glob="**/*.csv",
-        loader_cls=CSVLoader,
-        loader_kwargs={
-            "encoding": "utf-8",
-            "csv_args": {
-                "fieldnames": ["index", "title", "date", "author", "link", "content"],
-                "delimiter": ",",
-            },
-            "content_columns": "content",
+    csv_loader_ptt = CSVLoader(
+        file_path=os.path.join(directory_path, "ptt_cat.csv"),
+        encoding="utf-8",
+        csv_args={"fieldnames": ["index", "title", "date", "author", "link", "content"], "delimiter": ","},
+        content_columns="content",
+    )
+
+    csv_loader_CatTestResult = CSVLoader(
+        file_path=os.path.join(directory_path, "花花血檢結果.csv"),
+        encoding="utf-8",
+        csv_args={
+            "fieldnames": ["date", "test_item", "test_result", "test_range", "test_unit"],
+            "delimiter": ",",
         },
     )
 
-    documents = txt_loader.load() + csv_loader.load()
+    # pdf
+    pdf_loader = UnstructuredLoader(
+        file_path=os.path.join(directory_path, "花花檢查結果_20240608.pdf"),
+        streategy="hi_res",
+        partition_via_api=True,
+        coordinates=True,
+    )
+
+    documents = (
+        txt_loader.load() + csv_loader_ptt.load() + csv_loader_CatTestResult.load() + pdf_loader.load()
+    )
     logger.info(f"successfully loading {len(documents)} dcouments")
     return documents
 
@@ -59,7 +75,7 @@ def split_documents(documents, chunk_size=500, chunk_overlap=50):
 
 
 # create vector store
-def create_vector_store(splits, persist_directory: str):
+def create_vector_store(splits):
     logger.info("start creating vector store")
 
     # init embeddings
@@ -68,12 +84,16 @@ def create_vector_store(splits, persist_directory: str):
     )
 
     # init vector store
-    vector_store = Chroma(
-        collection_name="cat_care", embedding_function=embeddings, persist_directory=persist_directory
-    )
+    # vector_store = Chroma(
+    #     collection_name="cat_care", embedding_function=embeddings, persist_directory=persist_directory
+    # )
+    pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+    index = pc.Index("catlinebot")
+    vector_store = PineconeVectorStore(index=index, embeddings=embeddings)
     vector_store.add_documents(splits)
 
-    logger.info(f"vector store saved to {persist_directory}")
+    # logger.info(f"vector store saved to {persist_directory}")
+    logger.info("Documents successfully added to Pinecone")
 
     return vector_store
 
@@ -82,7 +102,7 @@ def main():
     DOCS_DIRECTORY = os.path.join(os.getcwd(), "input_docs")
     CHUNK_SIZE = 500
     CHUNK_OVERLAP = 50
-    DB_DIRECTORY = os.path.join(os.getcwd(), "cat_care_db")
+    # DB_DIRECTORY = os.path.join(os.getcwd(), "cat_care_db")
 
     try:
         # 1. load documents
@@ -92,7 +112,7 @@ def main():
         splits = split_documents(documents, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
 
         # 3. create vector store
-        _ = create_vector_store(splits, DB_DIRECTORY)
+        _ = create_vector_store(splits)
 
     except Exception as e:
         logger.error(f"Error: {str(e)}")
